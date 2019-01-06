3c980eb5dcf2132c7a2cb15b3d60c38876e5fe74
iiMohamedYT88 <46201203+iiMohamedYT88@users.noreply.github.com>
2019-01-06 18:49:16 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("531485426981142553")
+setInterval(function() {
+channel.send(`Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 32473 " ]
+Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 32474 " ]
+`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
